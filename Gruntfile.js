ba53b7456f323059d09628b8e2cff68b60e5105e
Sergey <grishinygrada@gmail.com>
2015-01-25 14:53:08 +0300

    setting grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,99 @@
 module.exports = function(grunt) {
-
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            js: {
-                src: ['./src/js/**/*.js'],
-                dest: './build/main.js',
+        jade: {
+            compile: {
+                options: {
+                    data: {
+                        debug: false
+                    }
+                },
+                files: {
+                    'dest/index.html': 'source/jade/index.jade'
+                }
             }
         },
+        /*----------------------------------------------------------*/
+        stylus: {
+            compile: {
+                files: {
+                    'ready/css/stylus.css': 'source/stylus/*.styl'
+                }
+            }
+        },
+        /*----------------------------------------------------------*/
+        coffee: {
+            compile: {
+                files: {
+                    'ready/js/coffee.js': 'source/coffee/*.coffee'
+                }
+            }
+        },
+        /*----------------------------------------------------------*/
+        concat: {
+            js: {
+                src: 'ready/js/**/*.js',
+                dest: 'dest/main.js',
+            },
+            css: {
+                src: 'ready/css/**/*.css',
+                dest: 'dest/style.css',
+            },
+        },
+        /*----------------------------------------------------------*/
+        watch: {
+            livereload: {
+                options: {
+                    livereload: true
+                },
+                files: ['dest/**/*'],
+            },
+            js: {
+                files: ['ready/js/**/*.js'],
+                tasks: ['concat:js'],
+            },
+            css: {
+                files: ['ready/css/**/*.css'],
+                tasks: ['concat:css'],
+            },
+            jade: {
+                files: ['source/jade/**/*.jade'],
+                tasks: ['jade'],
+            },
+            stylus: {
+                files: ['source/stylus/*.styl'],
+                tasks: ['stylus'],
+            },
+            coffee: {
+                files: ['source/coffee/*.coffee'],
+                tasks: ['coffee'],
+            },
+        },
+        /*----------------------------------------------------------*/
+        connect: {
+            server: {
+                options: {
+                    port: 3000,
+                    base: 'dest',
+                }
+            }
+        },
+        /*----------------------------------------------------------*/
     });
 
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-coffee');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
-    grunt.registerTask('default', ['concat']);
-
+    grunt.registerTask('default', [
+        'jade',
+        'stylus',
+        'coffee',
+        'concat:js',
+        'concat:css',
+        'connect',
+        'watch'
+    ]);
 };
